72885b7ee8ef4f2c9fa59c714d39e8939fb1fbe4
krutarth <52596504+krutarth-dev@users.noreply.github.com>
2023-05-31 15:28:49 +0530

    Create Answer5.py

---
+++ Assignment2/Answer5.py
@@ -0,0 +1,6 @@
+def maximumProduct(nums):
+    nums.sort()
+    n = len(nums)
+    return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1])
+nums = [1,2,3]
+print(maximumProduct(nums))
