0a742d334a3aa91485e1f6f7439bbe7361eee3f5
krutarth <52596504+krutarth-dev@users.noreply.github.com>
2023-07-16 00:11:38 +0530

    Create answer6.py

---
+++ assignment11/answer6.py
@@ -0,0 +1,15 @@
+def find_min(nums):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return nums[left]
+nums = [4, 5, 6, 7, 0, 1, 2]
+print(find_min(nums))  
