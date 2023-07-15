9b842a1e67da2111d942e8f360629d02a7af2fd4
krutarth <52596504+krutarth-dev@users.noreply.github.com>
2023-07-16 00:06:05 +0530

    Create answer2.py

---
+++ assignment11/answer2.py
@@ -0,0 +1,15 @@
+def find_peak_element(nums):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
+
+nums = [1, 2, 3, 1]
+print(find_peak_element(nums))  
